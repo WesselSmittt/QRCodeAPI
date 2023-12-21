2a2d4138a3529baee9d0b5532c24a8c1e875a0a8
timdegroot05 <336795@student.mboutrecht.nl>
2023-12-21 13:59:25 +0100

    jehtoch

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('QR codes') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Welkom op het dashboard") }}
+                     <form action="">
+                        <input type="text" name="url" id="url" placeholder="Enter URL" class="text-gray-900"><br><br>
+                         
+                        <input type="submit" value="Generate QR Code" class="">
+                     </form>
+                    
                 </div>
             </div>
         </div>
